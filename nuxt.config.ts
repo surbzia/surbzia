b4742bd14e29e2302480b5a3f3ba9a8d4220b675
surbzia <101662816+surbzia@users.noreply.github.com>
2025-08-12 19:59:53 +0500

    chanegs made

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,17 @@ export default defineNuxtConfig({
     autoImport: true,
   },
   devtools: { enabled: true },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Saleem-Ur-Rehman",
+      meta: [
+        { charset: "UTF-8" },
+        { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+      ],
+    },
+  },
 });
